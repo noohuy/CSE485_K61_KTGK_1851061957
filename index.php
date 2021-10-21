2b993a84c5374d6d86fc66ef27955cd9a8524cb9
noohuy1500 <noohuy1500@gmail.com>
2021-10-21 14:09:15 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hệ thống quản lý bệnh việnviện</title>
+  </head>
+  <body>
+  <h1 class="bg-warning">Hệ thống quản lý bệnh nhân</h1>
+  <main>
+        <!-- Hiển thị BẢNG DỮ LIỆU DANH BẠ CÁ NHÂN -->
+        <!-- Kết nối tới Server, truy vấn dữ liệu (SELECT) từ Bảng db_employees -->
+        <!-- Quy trình 4 bước -->
+        <a href="addEmployees.php" class="btn btn-success"><i class="fas fa-user-plus"></i> Thêm Nhân viên</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Mã bệnh nhân</th>
+                    <th scope="col">Tên</th>
+                    <th scope="col">Họ đệm</th>
+                    <th scope="col">Ngày sinh</th>
+                    <th scope="col">Giới tính</th>
+                    <th scope="col">Số điện thoại</th>
+                    <th scope="col">Sửa thông tin</th>
+                    <th scope="col">Xóa nhân viên</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Đoạn này thay đổi theo Dữ liệu trong CSDL -->
+                <?php
+                    // Quy trình 4 bước
+                    // Bước 01: Đã tạo sẵn, gọi lại thôi
+                    include 'connect.php';
+                    // Bước 02: Thực hiện TRUY VẤN
+                    $sql = "SELECT e.patientid,e.firstname,e.lastname,e.dateofbirth, e.gender, e.mobile FROM benhnhan e";
+                    $result = mysqli_query($conn,$sql); //Lưu kết quả trả về vào result
+                    // Bước 03: Phân tích và xử lý kết quả
+                    if(mysqli_num_rows($result)>0){
+                        while($row=mysqli_fetch_assoc($result)){
+                            echo '<tr>';
+                            echo '<th scope="row">'.$row['patientid'].'</th>';
+                            echo '<td>'.$row['firstname'].'</td>';
+                            echo '<td>'.$row['lastname'].'</td>';
+                            echo '<td>'.$row['dateofbirth'].'</td>';
+                            echo '<td>'.$row['gender'].'</td>';
+                            echo '<td>'.$row['mobile'].'</td>';
+                            echo '<td><a href="editEmployee.php?id='.$row['emp_id'].'"><i class="fas fa-user-edit"></i></a></td>';
+                            echo '<td><a href="deleteEmployee.php?id='.$row['emp_id'].'"><i class="fas fa-user-times"></i></a></td>';
+                            echo '</tr>';
+                        }
+                    }
+                ?>
+                
+                <!-- Đoạn này thay đổi theo Dữ liệu trong CSDL -->
+            </tbody>
+            </table>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
